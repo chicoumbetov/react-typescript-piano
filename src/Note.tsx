9666a100d308059e43bdbe768d08fa262842adee
chicoumbetov <shyngys-maaan@mail.ru>
2021-01-27 00:13:06 +0100

    Style of piano button

--- src/Note.tsx
+++ src/Note.tsx
@@ -3,9 +3,27 @@ import styled from 'styled-components';
 
 const Black = styled.button`
     background: black;
+    width: 40px;
+    height: 130px;
+    position: absolute;
+    margin: 1px;
+    margin-left: -20px;
+
+    :active {
+        background: #333;
+    }
 `
 const White = styled.button`
     background: white;
+    width: 60px;
+    height: 200px;
+    border: 1px solid black;
+    box-shadow: 2px 5px;
+    margin: 1px;
+
+    :active {
+        background: #eee;
+    }
 `
 
 type Props = {
@@ -13,12 +31,12 @@ type Props = {
     note: string;
 }
 
-const Note= ({color, note}: Props) => 
+const Note = ({ color, note }: Props) =>
     color === 'white' ? (
         <White value={note} />
 
     ) : (
-        <Black value={note} />
-    )
+            <Black value={note} />
+        )
 
 export default Note;
--- src/Octave.tsx
+++ src/Octave.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import { NoteType } from './helpers';
 import Note from './Note';
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+`
 
 type Props = {
     notes: NoteType[];
 }
 
 const Octave = ({ notes }: Props) => (
-    <div>
-        {notes.map((element: NoteType) => (
-            <Note
-                key={element.note}
-                color={element.color}
-                note={element.note}
-            />
-        ))}
-    </div>
+    <Wrapper>
+        <div>
+            {notes.map((element: NoteType) => (
+                <Note
+                    key={element.note}
+                    color={element.color}
+                    note={element.note}
+                />
+            ))}
+        </div>
+    </Wrapper>
+
 )
 
 export default Octave;
